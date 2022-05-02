60b047054c2043bbf734580b2a1904351ced66ac
Rteekasingh <rteekasingh@gmail.com>
2022-05-02 03:50:38 -0400

    Added reaction.js

---
+++ models/reaction.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+// This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
